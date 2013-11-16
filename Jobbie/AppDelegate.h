da7906de7a9eca70b4244bc2af07de04a35d2d00
Daniel Whitcomb <danrwhitcomb@gmail.com>
2013-11-16 14:53:37 -0500

    Initial Commit

---
+++ Jobbie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jobbie
+//
+//  Created by Dan Whitcomb on 11/16/13.
+//  Copyright (c) 2013 Jobbie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

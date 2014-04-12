4e0fc5f4c7cb9da42f85dc9516821e7f208b382f
Daniel Whitcomb <danrwhitcomb@gmail.com>
2014-04-12 15:29:34 -0500

    Ignored venv from repo

--- Server/JobbieServer/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/danrwhitcomb/Dropbox/Jobbie/DevWork/Jobbie/Server/JobbieServer/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
